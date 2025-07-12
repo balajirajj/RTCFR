0da2ae3448a9736ca59110b76754af3e3d35cc90
balajirajj <balajirajj@gmail.com>
2025-07-12 07:37:06 +0530

    Add files via upload

---
+++ NameList.py
@@ -0,0 +1,6 @@
+# Store 5 names in a list
+names = ["Balaji", "Rekha", "Sowjanya", "Joshika", "Sakthi"]
+
+# Print each name with its length
+for name in names:
+    print(f"Name: {name}, Length: {len(name)}")
